f54b1f7a1e9c75224d64d05355cbeb33846670cd
Vlad Duma <vlad.duma@deplabs.com>
2020-10-15 21:36:06 +0300

    fix : add body parser

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+var textParser = bodyParser.text()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.post('/postData', (req, res) => {
-    console.log(req.headers);
-    res.send(`received data with headers: ${JSON.stringify(req.headers)}, url params: ${JSON.stringify(req.query)}, body: ${JSON.stringify(req.body)}`);
+app.post('/postData', textParser, (req, res) => {
+    res.send(`received data with headers: ${JSON.stringify(req.headers)}, url params: ${JSON.stringify(req.query)}, body: ${req.body}`);
 })
 
 app.listen(port, () => {
